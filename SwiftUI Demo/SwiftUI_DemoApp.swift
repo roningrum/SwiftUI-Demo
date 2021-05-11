aa9e175ad6e68adbbd1b883c036ebb17df94a582
roningrum <rizkioktavianingrum@gmail.com>
2021-05-11 08:13:02 +0700

    learning swift UI

---
+++ SwiftUI Demo/SwiftUI_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_DemoApp.swift
+//  SwiftUI Demo
+//
+//  Created by okta on 11/05/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
